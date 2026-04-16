891b275b739535757cd61931a828f5034af54f2f
L.A.S <l.azana@edu.uah.es>
2026-04-16 16:37:02 +0200

    Movimiento de archivos

---
+++ assets/Mapas/Infierno.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Infierno" tilewidth="40" tileheight="40" tilecount="646" columns="34">
+ <image source="ImagenInfierno.png" width="1371" height="768"/>
+</tileset>
