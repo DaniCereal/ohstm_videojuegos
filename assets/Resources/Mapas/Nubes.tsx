0696281d5940f654abe8e4914974bd46e555669c
L.A.S <l.azana@edu.uah.es>
2026-04-26 12:00:49 +0200

    Mapa Cielo creado,puede falta alguna cosilla pero seria la base

--- assets/Resources/Mapas/Nubes.tsx
+++ assets/Resources/Mapas/Nubes.tsx
@@ -1,2 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Nubes" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.10" tiledversion="1.12.1" name="Palaforma" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="Palaforma.png" width="32" height="32"/>
+</tileset>
