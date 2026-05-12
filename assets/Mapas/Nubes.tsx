4b51f5c34c330f2161c60cb64c4cce217e7522fa
L.A.S <l.azana@edu.uah.es>
2026-05-12 10:05:32 +0200

    Space to keep new map

--- assets/Mapas/Nubes.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Palaforma" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="Palaforma.png" width="32" height="32"/>
-</tileset>
