2003a0c933c92c37e48b1ace29edec77b6ff4c50
L.A.S <l.azana@edu.uah.es>
2026-05-14 15:42:44 +0200

    Mapa eliminados

--- assets/Mapas/Cielo abajo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Cielo abajo" tilewidth="32" tileheight="32" tilecount="920" columns="40">
- <image source="Cielo abajo.png" width="1280" height="736"/>
-</tileset>
