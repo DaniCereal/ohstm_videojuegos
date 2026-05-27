cdf6e086457a459d84dbdcb240fae63ad58f7b14
Brayan <brayan.cotoara@edu.uah.es>
2026-05-27 23:00:30 +0200

    Mapa con texturas corregidas

---
+++ assets/Niveles/Tileset_Cielo_girado.tsx
@@ -0,0 +1,364 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Tileset_Cielo_girado" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="../../../Desktop/Tileset_Cielo_girado.png" width="256" height="384"/>
+ <tile id="2" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="21" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="35" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="38" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="39" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="42" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="43" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="44" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="45" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="46" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="47" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="50" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="51" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="53" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="58" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="60" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="61" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="62" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="63" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="66" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="67" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="68" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="69" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="70" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="71" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="74" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="75" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="76" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="77" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="78" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="79" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="82" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="83" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="84" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="85" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="86" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="87" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="90" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="91" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="92" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="93" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94" type="Platform">
+  <objectgroup>
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="95" type="Platform">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/Niveles/Tileset_Tierra_girado.tsx
@@ -0,0 +1,365 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Tileset_Tierra_girado" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="../../../Desktop/Tileset_Tierra_girado.png" width="256" height="384"/>
+ <tile id="2">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="29">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="47">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="63">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="74">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="75">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="76">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="77">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="78">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="87">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="90">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup>
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="95">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Platform" x="0" y="0" width="32" height="32"/>
+   <object id="2" type="Platform" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
