2b57be09ce8e402719dc4e7288f7cb31bff0aadd
Brayan <brayan.cotoara@edu.uah.es>
2026-05-31 05:08:43 +0200

    Mapa completo y decorado

---
+++ assets/Niveles/Decoracion/Arboles_sin_hojas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Arboles_sin_hojas" tilewidth="32" tileheight="32" tilecount="63" columns="7">
+ <image source="../../Mapas/Decoracion/Arboles_sin_hojas.png" width="246" height="300"/>
+</tileset>
---
+++ assets/Niveles/Decoracion/Hierba.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Hierba" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="../../Mapas/Decoracion/Hierba.png" width="96" height="128"/>
+</tileset>
